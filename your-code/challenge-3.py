392311aa6f4a46afef11f1db78c7b2f6174ee708
Erwan de Boisjolly <erwan.deboisjolly@gmail.com>
2020-07-07 18:25:19 +0200

    submit challenge 3

--- your-code/challenge-3.py
+++ your-code/challenge-3.py
@@ -13,12 +13,8 @@ Refactor the code based on what you have learned about code simplicity and effic
 """
 
 def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                    solutions.append([x, y, z])
+    solutions = [[x, y, z] for x in range(5, X) for y in range(4, X) for z in range(3, X) if (x*x==y*y+z*z)]
+               
     m = 0
     for solution in solutions:
         if m < max(solution):
@@ -27,4 +23,7 @@ def my_function(X):
 
 X = input("What is the maximal length of the triangle side? Enter a number: ")
 
-print("The longest side possible is " + str(my_function(int(X))))
+try:
+    print("The longest side possible is " + str(my_function(int(X))))
+except:
+    print('Sorry, next time, give an integer number (with digits) larger than 5')
